fa1d054a45d411a12123cb86119d977a72d88e17
k1b3r74rd <54848375+k1b3r74rd@users.noreply.github.com>
2019-10-04 19:53:08 +0700

    Add files via upload

--- vk_avatar_changer.py
+++ vk_avatar_changer.py
@@ -6,7 +6,10 @@ import random
 import requests
 import vk
 
-token_user = r'1c53091fae5b8ae2f2c25bf2d940e1d191cb783d6bfd2b6fcbb8b6068879f582d2fb46780ccbd50381398'
+# Insert your user token and profile/group ID accordingly. Remember about minus before group ID.
+user_token = '1c53091fae5b8ae2f2c25bf2d940e1d191cb783d6bfd2b6fcbb8b6068879f582d2fb46780ccbd50381398'
+owner_id = -187198717
+
 API_version = 5.101
 
 path = Path('pictures')
@@ -15,14 +18,15 @@ pictures = []
 for f in pictures_list:
     pictures.append(f)
 
-session = vk.Session(access_token=token_user)
+session = vk.Session(access_token=user_token)
 vk_api = vk.API(session, v=API_version)
 
-upload_url = vk_api.photos.getOwnerPhotoUploadServer(owner_id=-187198717)['upload_url']
+upload_url = vk_api.photos.getOwnerPhotoUploadServer(owner_id=owner_id)['upload_url']
 
 while(True):
-    time.sleep(300)
     x = random.randint(0, len(pictures) - 1)
     image = {'photo': open(path/pictures[x], 'rb')}
     ur = requests.post(upload_url, files=image).json()
     vk_api.photos.saveOwnerPhoto(server=ur['server'], photo=ur['photo'], hash=ur['hash'])
+    print('Avatar has changed successfully.')
+    time.sleep(300)
